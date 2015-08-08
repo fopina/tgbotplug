d4e8abca524731cb8afb53ee4efaa66988feb0a9
Filipe Pina <fopina@skmobi.com>
2015-08-08 16:31:19 +0100

    version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except:
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-__version__ = '1.0.2'
+__version__ = '1.0.3'
 
 setup(
     name='tgbotplug',
