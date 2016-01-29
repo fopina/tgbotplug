c115733b03f2e23ddcdecfce588d1a6a1e5bde91
Filipe Pina <fopina@gmail.com>
2016-01-29 17:06:52 +0000

    test for #31

--- tests/test_issues.py
+++ tests/test_issues.py
@@ -1,16 +1,15 @@
-from tgbot import plugintest
+from tgbot import plugintest, pluginbase
 from tgbot.botapi import Message
 from sample_plugin import TestPlugin
+
+import threading
+import time
 
 
 class IssuesTest(plugintest.PluginTestCase):
     def setUp(self):
         self.plugin = TestPlugin()
-        self.bot = self.fake_bot(
-            '',
-            plugins=[self.plugin],
-        )
-        self.received_id = 1
+        self.bot = self.fake_bot('', plugins=[self.plugin])
 
     def test_user_update(self):
         """Update user information (issue #22)"""
@@ -81,3 +80,70 @@ class IssuesTest(plugintest.PluginTestCase):
             m.assert_called_with(40, 'in_message must be instance of Message, discarded: %s', ('str',))
             self.plugin.need_reply(None, Message.from_result({}), out_message='str')
             m.assert_called_with(40, 'out_message must be instance of Message, discarded: %s', ('str',))
+
+    def test_atomic_operation(self):
+        """Test atomic operation (issue #31)"""
+        import tempfile
+        import os
+        _, v = tempfile.mkstemp()
+        self.plugin = AtomicTestPlugin()
+        self.bot = self.fake_bot(
+            '', plugins=[self.plugin],
+            # TODO: fix this for shared in-memory - it seems to always be creating the file even with mode=memory...
+            db_url='sqlite:///%s?mode=memory&cache=shared' % v
+        )
+        self.bot.setup_db()
+
+        self.receive_message('/atomic_fail')
+        self.receive_message('/atomic_fail')
+        time.sleep(0.05)
+        # second one should fail or return 2, not 1...
+        self.assertReplied('Value: 1')
+        self.assertReplied('Value: 1')
+
+        # try the proper one
+        self.receive_message('/atomic_good')
+        self.receive_message('/atomic_good')
+        time.sleep(0.05)
+        # second call failed to update atomically
+        # replies will not be ordered due to threading...
+        r = [self.last_reply(), self.last_reply()]
+        self.assertIn('Value: 2', r)
+        self.assertIn('Failed update', r)
+        os.unlink(v)
+
+
+class AtomicTestPlugin(TestPlugin):
+    def list_commands(self):
+        return [
+            pluginbase.TGCommandBase('atomic_fail', self.atomic_fail, ''),
+            pluginbase.TGCommandBase('atomic_good', self.atomic_good, '')
+        ]
+
+    def _update(self, chat_id):
+        x = self.read_data(chat_id)
+        if not x:
+            x = 0
+        x += 1
+        time.sleep(0.01)
+        self.save_data(chat_id, obj=x)
+        self.bot.send_message(chat_id, 'Value: %d' % x)
+
+    def _atomic_update(self, chat_id):
+        try:
+            with self.bot.db.atomic():
+                x = self.read_data(chat_id)
+                if not x:
+                    x = 0
+                x += 1
+                time.sleep(0.01)
+                self.save_data(chat_id, obj=x)
+                self.bot.send_message(chat_id, 'Value: %d' % x)
+        except:
+            self.bot.send_message(chat_id, 'Failed update')
+
+    def atomic_fail(self, message, text):
+        threading.Thread(target=self._update, args=(message.chat.id,)).start()
+
+    def atomic_good(self, message, text):
+        threading.Thread(target=self._atomic_update, args=(message.chat.id,)).start()
